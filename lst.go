dcb17da3eaf2e98c1ac98a43d138f8c2736f078e
Guillaume <guillaume@ophane.net>
2022-04-08 09:40:54 +0200

    v2

--- lst.go
+++ lst.go
@@ -1,70 +1,92 @@
 package main
 
 import (
-	//"fmt"
-	//"strconv"
+	"fmt"
 	"os"
 	"time"
 
 	g "github.com/AllenDang/giu"
 )
 
-var content string
-var t0 time.Time
-var t float64
-var text, s string
-var filename = "timecode.txt"
+var (
+	t0                       time.Time
+	content, text, timestamp string
+	filename                 = "timecode.txt"
+	ticker                   *time.Ticker
+	auto                     bool
+)
 
-func plusUn() {
-	t0 = t0.Add(-time.Second)
-}
-
-func moinsUn() {
+func plusOne() {
 	t0 = t0.Add(time.Second)
 }
 
-func Raz() {
-	t0 = time.Now()
+func minusOne() {
+	t0 = t0.Add(-time.Second)
 }
 
-func Newline() {
-	text = text + s + " " + content + "\n"
+func resetTicker() {
+	t0, _ = time.Parse("05", "0")
+}
+
+func startTicker() {
+	ticker.Reset(time.Second)
+}
+
+func stopTicker() {
+	ticker.Stop()
+}
+
+func newLine() {
+	text = text + timestamp + " " + content + "\n"
 	content = ""
 }
 
-func Savetofile() {
+func saveTofile() {
 	f, _ := os.Create("timecode.txt")
 	defer f.Close()
 	f.WriteString(text)
 }
+
+func tickerUpdate() {
+	for {
+		<-ticker.C
+		t0 = t0.Add(time.Second)
+		g.Update()
+	}
+}
+
 func loop() {
-	t = time.Since(t0).Seconds()
-	e := time.Duration(int(t)) * time.Second
-	t1 := time.Unix(int64(e.Seconds()), int64(t))
-	s = t1.Format("03:04:05")
+	timestamp = t0.Format("15:04:05")
 	g.SingleWindow().Layout(
 		g.Row(
-			g.Label(s),
-			g.Button("+1").OnClick(plusUn),
-			g.Button("-1").OnClick(moinsUn),
-			g.Button("raz").OnClick(Raz),
+			g.Label(timestamp),
+			g.Button("+1").OnClick(plusOne),
+			g.Button("-1").OnClick(minusOne),
+			g.Button("start").OnClick(startTicker),
+			g.Button("stop").OnClick(stopTicker),
+			g.Button("reset").OnClick(resetTicker),
+			g.Checkbox("auto", &auto),
 		),
+		/*g.Custom(func() {
+			if auto {
+				g.SetKeyboardFocusHere()
+			}
+		}),*/
 		g.InputText(&content).Size(g.Auto),
-		g.Event().OnKeyPressed(g.KeyEnter, Newline),
-		g.Event().OnKeyReleased(g.KeyEnter, g.SetKeyboardFocusHere),
-		g.Button("New").OnClick(Newline),
-		g.InputTextMultiline(&text).Size(g.Auto, g.Auto-50),
+		g.Event().OnKeyPressed(g.KeyEnter, newLine),
+		g.Button("New").OnClick(newLine),
+		g.InputTextMultiline(&text).Size(g.Auto, g.Auto-30),
 		g.Row(
-			g.Button("Save").OnClick(Savetofile),
+			g.Button("Save").OnClick(saveTofile),
 			g.InputText(&filename),
 		),
 	)
-	g.Update()
-
+	fmt.Println(auto)
 }
 
 func main() {
-	t0 = time.Now()
-	wnd := g.NewMasterWindow("Live Shill Timecode", 500, 350, g.MasterWindowFlagsFloating)
+	ticker = time.NewTicker(time.Second)
+	go tickerUpdate()
+	wnd := g.NewMasterWindow("Live Shill Timecode", 500, 500, g.MasterWindowFlagsFloating)
 	wnd.Run(loop)
 }
